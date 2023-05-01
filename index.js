dd3af003d1139d8b47b01fd571409995f95674bd
eagletvn <116724924+eagletvn@users.noreply.github.com>
2023-05-01 16:56:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 const options = {
-  originWhitelist: ['http://localhost:3000'],
-  requireHeader: ['origin', 'x-requested-with'],
+  originWhitelist: [],
+  //requireHeader: ['origin', 'x-requested-with'],
   removeHeaders: ['cookie', 'cookie2']
 };
 const cors_proxy = require('cors-anywhere').createServer(options);
