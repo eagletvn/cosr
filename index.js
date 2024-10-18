40d0711cded9f815e81193e0cdd8a138893e6b46
eagletvn <116724924+eagletvn@users.noreply.github.com>
2024-10-18 18:33:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 #!/usr/bin/env node
 'use strict';
 const options = {
-  originWhitelist: [],
-  //requireHeader: ['origin', 'x-requested-with'],
-  //requireHeader: ['Content-Type', 'application/xml'],
+  originWhitelist: [], // Membolehkan semua asal (origin)
   removeHeaders: ['cookie', 'cookie2']
 };
-const cors_proxy = require('cors-anywhere').createServer{
+const cors_proxy = require('cors-anywhere').createServer(options);
+
 module.exports = (req, res) => {
   console.log('headers', req.headers);
   console.log('url', req.url);
+
+  // Cek jika URL tidak valid atau root
+  if (!req.url || req.url === "/") {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Invalid request. No URL provided.');
+    return;
+  }
+
+  // Jika URL valid, teruskan ke cors-anywhere proxy
   cors_proxy.emit('request', Object.assign(req, {url: req.url.replace(':/', '://')}), res);
 };
+
