2145316a31dbd46803c0b236f597513932cfe81b
eagletvn <116724924+eagletvn@users.noreply.github.com>
2024-10-18 19:24:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 #!/usr/bin/env node
 'use strict';
-
-const cors_proxy = require('cors-anywhere');
-
-// Opsi server CORS-Anywhere
 const options = {
-  originWhitelist: [], // Mengizinkan semua origin
-  removeHeaders: ['cookie', 'cookie2'],
-  requireHeader: [], // Memastikan tidak ada header yang diperlukan
+  originWhitelist: [],
+  //requireHeader: ['origin', 'x-requested-with'],
+  //requireHeader: ['Content-Type', 'application/xml'],
+  removeHeaders: ['cookie', 'cookie2']
 };
-
-// Membuat server CORS proxy dengan opsi
-const server = cors_proxy.createServer(options);
-
-// Menangani permintaan
+const cors_proxy = require('cors-anywhere').createServer{
 module.exports = (req, res) => {
   console.log('headers', req.headers);
   console.log('url', req.url);
-
-  // Memeriksa apakah URL adalah root
-  if (req.url === "/" || req.url === "") {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found'); // Ubah pesan sesuai keinginan
-    return;
-  }
-
-  // Jalankan proxy
-  server.emit('request', Object.assign(req, { url: req.url.replace(':/', '://') }), res);
+  cors_proxy.emit('request', Object.assign(req, {url: req.url.replace(':/', '://')}), res);
 };
